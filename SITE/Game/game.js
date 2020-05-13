4910768170be647e1d48180a03564def6e3ea41b
Greccu <56735903+Greccu@users.noreply.github.com>
2020-05-13 20:07:58 +0300

    Update game.js

--- SITE/Game/game.js
+++ SITE/Game/game.js
@@ -1,4 +1,4 @@
-//json-server --watch desktop/site/game/quiz.json
+//   json-server --watch desktop/site/game/quiz.json
 
 function renderQuestion(){
     let q = questions[runningQuestion];
@@ -89,7 +89,7 @@ function restartProgress(){
 }
 
 function renderProgress(){
-    for(let qIndex = 0; qIndex <= quizlenght; qIndex++){
+    for(let qIndex = 0; qIndex <= quizlength; qIndex++){
         progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
     }
 }
@@ -102,7 +102,7 @@ function renderCounter(){
     }else{
         count = 0;
         answerIsWrong();
-        if(runningQuestion < quizlenght){
+        if(runningQuestion < quizlength){
             runningQuestion++;
             renderQuestion();
         }else{
@@ -121,7 +121,7 @@ function checkAnswer(answer){
         answerIsWrong();
     }
     count = 0;
-    if(runningQuestion < quizlenght){
+    if(runningQuestion < quizlength){
         runningQuestion++;
         renderQuestion();
     }else{
@@ -214,9 +214,7 @@ function updateQuesiton() {
     var id = document.getElementById("input-id").value;
     var name = document.getElementById("input-update-name").value;
     var img = document.getElementById("input-update-img").value;
-    var newDog = {
-        name: name,
-        img: img
+    var newQuestion = {
     }
 
     console.log(newDog, id)
@@ -231,7 +229,6 @@ function updateQuesiton() {
         window.location.reload();
     })
 }
-
 
 const butoane = document.getElementById("buttoncontainer");
 const start = document.getElementById("start");
@@ -248,9 +245,12 @@ const progress = document.getElementById("progress");
 const scorediv = document.getElementById("scoreContainer");
 const submit = document.getElementById("quizsubmit");
 const submitinterface = document.getElementById("quizinput");
+const loginform = document.getElementById("login");
+const loginbut = document.getElementById("loginbut");
+const registerbut = document.getElementById("registerbut");
 
 animation = anime.timeline();
-const quizlenght = 10 - 1;
+const quizlength = 10 - 1;
 let runningQuestion = 0;
 let count = 0;
 const questionTime = 10; // seconds
@@ -267,4 +267,112 @@ questionrequest.send();
 
 start.addEventListener('click',prestartQuiz);
 submit.addEventListener('click',submitquestion);
+loginbut.addEventListener('click',login);
+registerbut.addEventListener('click',change_to_register);
 
+/////////////////////
+
+var users;
+var usersrequest = new XMLHttpRequest();
+usersrequest.open('GET','http://localhost:3000/users');
+usersrequest.onload = function(){
+    users = JSON.parse(usersrequest.responseText);
+}
+usersrequest.send();
+
+function login(){
+    let us = document.getElementById("username").value;
+    let pas = document.getElementById("password").value;
+    let ok = 0;
+    for(let i = 0; i < users.length; i++){
+        if(users[i].username == us && users[i].password == pas){
+            ok = 1;
+            break;
+        }
+    }
+    if(ok){
+        butoane.style.display = 'grid';
+        loginform.style.display = 'none';
+    }
+    else{
+        let warning = document.getElementById("warning");
+        warning.innerHTML = "<p> USERNAME OR PASSWORD ARE INCORRECT</p>";
+    }
+}
+
+
+
+function change_to_register(){
+    document.getElementById("warning").innerHTML = "";
+    document.getElementById("registerform").style.display = "block";
+    loginbut.removeEventListener('click',login);
+    registerbut.removeEventListener('click',change_to_register);
+    loginbut.addEventListener('click',change_to_login);
+    registerbut.addEventListener('click',register);
+
+}
+
+
+function change_to_login(){
+    document.getElementById("warning").innerHTML = "";
+    document.getElementById("registerform").style.display = "none";
+    loginbut.removeEventListener('click',change_to_login);
+    registerbut.removeEventListener('click',register);
+    loginbut.addEventListener('click',login);
+    registerbut.addEventListener('click',change_to_register);
+
+}
+
+function register(){
+    let us = document.getElementById("username").value;
+    let pas = document.getElementById("password").value;
+    let pasc = document.getElementById("confirmpassword").value;
+    let warning = document.getElementById("warning");
+    if(pas != pasc){
+        warning.innerHTML = "<p>PASSWORDS DO NOT MATCH</p>"
+    }
+    else{
+        let ok = 1;
+        for(let i = 0; i < users.length; i++){
+            if(users[i].username == us){
+                ok = 0;
+                break;
+            }
+        }
+
+        if(ok == 0){
+            warning.innerHTML = "<p>USERNAME ALREADY EXISTS</p>"
+        }
+        else{
+            var newUser = {
+                "username":us,
+                "password":pas
+            }
+            console.log(newUser);
+            fetch('http://localhost:3000/users', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        }).then(function(response) {
+            console.log(response);
+    })
+        }
+    }
+    /*
+    var newQuestion = {
+        
+    }
+    newQuestion.answers[correctanswer-1][1] = true;
+    fetch('http://localhost:3000/questions', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newQuestion)
+    }).then(function(response) {
+        console.log(response);
+    })
+    */
+}
